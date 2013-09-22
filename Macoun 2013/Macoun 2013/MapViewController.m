0afe6d8870e90315459b8a40a76d09c4dd6daa5b
Claus Höfele <choefele@ebay.com>
2013-09-22 10:39:31 +0200

    Added annotation

--- Macoun 2013/Macoun 2013/MapViewController.m
+++ Macoun 2013/Macoun 2013/MapViewController.m
@@ -22,6 +22,13 @@
     CLLocationCoordinate2D location = CLLocationCoordinate2DMake(52.516221, 13.377829);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location, 1000, 1000);
     self.mapView.region = region;
+    
+    // Add pin for Brandenburg Gate
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    annotation.coordinate = location;
+    annotation.title = @"Berlin";
+    annotation.subtitle = @"Brandenburger Tor";
+    [self.mapView addAnnotation:annotation];
 }
 
 @end
